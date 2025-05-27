7e3a6a99e0525d4ea1a0d19b6f1a2c5ac915c64e
idiq-3 <shidiq361@gmail.com>
2025-05-27 14:15:32 +0800

    nama dashboard

--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Brand -->
-    <a class="navbar-brand ps-3" href="{{ route('dashboard') }}">Dashboard Idiq</a>
+    <a class="navbar-brand ps-3" href="{{ route('dashboard') }}">Dashboard Shidiq</a>
 
     <!-- Sidebar Toggle -->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#">
